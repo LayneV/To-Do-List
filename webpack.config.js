638a82a87109ffad1479ba10c983226d0acde454
LayneV <152360779+LayneV@users.noreply.github.com>
2025-09-24 18:39:09 -0500

    chore: Initial project setup with webpack and basic file structure

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.[contenthash].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", 
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    hot: true,
+    open: true,
+    port: 3000,
+  },
+  mode: "development",
+};
