b928a8c5c0cfb3d01926c35240f8559da745c425
LayneV <152360779+LayneV@users.noreply.github.com>
2025-09-24 19:41:10 -0500

    feat(logic): Implement central logic to manage projects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import "./styles.css";
+import { createToDoItems } from "./todo";
+import { Project } from "./project";
+
+let todoList = [];
+
+export function getProjects() {
+    return todoList;
+}
+
+function addNewProject(project) {
+  todoList.push(project);
+}
+
+function findProject(project) {
+  const foundProject = todoList.find((p) => p.name === project.name);
+
+  if (foundProject) {
+    console.log(foundProject);
+  } else {
+    console.log("Project does not exist");
+  }
+}
+
+let houseProjects = new Project("House Chores");
+addNewProject(houseProjects);
+let kitchen = createToDoItems(
+  "Clean kitchen",
+  "Wash the dishes",
+  "Tomorrow",
+  "Urgent"
+);
+
+houseProjects.addTodo(kitchen);
+console.log(todoList[0]);
